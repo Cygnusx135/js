44922dded442f1d8ecfd89f70ebb8b7b6b4c443e
Cygnusx135 <harihk9705@gmail.com>
2023-12-18 21:57:50 +0530

    operations and conversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -18,3 +18,52 @@ let someNumber = 10;
 let stringValue = String(someNumber);
 console.log(typeof stringValue);
 console.log(stringValue);
+
+// ******* Operations *******
+
+let value = 3;
+let negativeValue = -value;
+console.log(negativeValue);
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+// console.log(2 ** 2)
+// console.log(2 << 2)
+// console.log(2 >> 2)
+// console.log(2 >>> 2)
+// console.log(2 | 2)
+// console.log(2 & 2)
+// console.log(2 ^ 2)
+// console.log(~2)
+// console.log(2 == 2)
+// console.log(2!= 2)
+// console.log(2 === 2)
+// console.log(2!== 2)
+// console.log(2 < 2)
+// console.log(2 > 2)
+// console.log(2 <= 2)
+// console.log(2 >= 2)
+
+let str1 = 'Hari ';
+let str2 = 'Kiran';
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log('1' + 2);
+// console.log(1 + '2');
+// console.log(1 + 2 + '3');
+// console.log(1 + 2 + '3' + 4);
+// console.log('1' + 2 + 2);
+
+let x = 3;
+let z = x++;
+console.log(x);
+console.log(z);
+
+let y = 3;
+let w = ++y;
+console.log(y);
+console.log(w);
