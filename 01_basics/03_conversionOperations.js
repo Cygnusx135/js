825afd536c3ebc1787d12222a01df1637bd04b6b
Cygnusx135 <harihk9705@gmail.com>
2023-12-18 21:19:56 +0530

    type conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,20 @@
+let score = '100';
+console.log(typeof score);
+console.log(typeof '100');
+//Number conversion
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//boolean conversion
+let isLoggedIn = 1;
+let booleanValue = Boolean(isLoggedIn);
+console.log(typeof booleanValue);
+console.log(booleanValue);
+//1 => true; 0 => false
+
+//string conversion
+let someNumber = 10;
+let stringValue = String(someNumber);
+console.log(typeof stringValue);
+console.log(stringValue);
