d8c73bbd6d717debeedc2c8bc4408ee55bc78b96
Cygnusx135 <harihk9705@gmail.com>
2023-12-18 22:02:20 +0530

    comparision 2

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -2,3 +2,10 @@ console.log(2 > 1); // boolean output
 console.log(2 < 1);
 console.log(2 >= 1);
 console.log(2 <= 1);
+
+console.log('2' > 1);
+console.log('2' < 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
