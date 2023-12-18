5b9c6d73af8ec963ee96123cf5ed238d5b677712
Cygnusx135 <harihk9705@gmail.com>
2023-12-18 22:06:17 +0530

    comparision_3

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,11 +1,20 @@
 console.log(2 > 1); // boolean output
-console.log(2 < 1);
-console.log(2 >= 1);
-console.log(2 <= 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 <= 1);
 
-console.log('2' > 1);
-console.log('2' < 1);
+// console.log('2' > 1);
+// console.log('2' < 1);
 
-console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// == normal equality
+console.log(2 == '2'); //true
+// === strict equality
+console.log(2 === '2'); //false
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
