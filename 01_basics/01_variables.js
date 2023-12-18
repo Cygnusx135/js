5b4cd99b3b6f4b2e64c52fc27e15e2abc0f42760
Cygnusx135 <harihk9705@gmail.com>
2023-12-18 20:30:36 +0530

    first commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1234567890;
+let accountEmail = 'harihk@google.com';
+var accountPassword = '12345';
+accountCity = 'Virtual City';
+let accountState;
+
+// accountId = 2;
+accountEmail = 'hk@11g.com';
+accountPassword = '34567';
+accountCity = 'Virtual City 2';
+
+console.table([
+  accountId,
+  accountEmail,
+  accountPassword,
+  accountCity,
+  accountState,
+]);
