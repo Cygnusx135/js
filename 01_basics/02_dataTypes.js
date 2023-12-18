02ed4921b0d66b2705eba02ea8a9efc689c37c86
Cygnusx135 <harihk9705@gmail.com>
2023-12-18 21:02:14 +0530

    datatypes commit

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+'use script'; //treat all JS code as newer version
+
+// alert('Hello World!');
+
+let name = 'Hari';
+let age = 21;
+let isLoggedIn = true;
+
+//number
+//bigint
+//string
+//boolean
+//null =>standalone value
+//undefined =>no value assigned
+//symbol =>unique value
+
+//object
+console.log(typeof 'you');
+console.log(typeof age);
+console.log(typeof null);
