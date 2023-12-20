da0d8a34b25646fc1a74d347241a7fb93041eab6
Cygnusx135 <harihk9705@gmail.com>
2023-12-20 20:36:34 +0530

    math and num

---
+++ 01_basics/06_numsAndMath.js
@@ -0,0 +1,19 @@
+const balance = 1000;
+console.log(balance);
+
+const score = new Number(100);
+console.log(score);
+
+console.log(score.toString().length);
+console.log(score.toFixed(2));
+
+const otherScore = 23.8966;
+console.log(otherScore.toPrecision(2));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString());
+
+//******** MATHS *******
+
+console.log(Math);
+console.log(Math.PI);
